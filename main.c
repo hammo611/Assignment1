57a7fbdd8009e0c77c3837fc9c0dc615fc8c21cf
alex hamilton <alexhamilton997@gmail.com>
2019-04-02 01:09:59 +0000

    Stores alphabet in an array

--- main.c
+++ main.c
@@ -2,7 +2,16 @@
 
 int main()
 {
+    int i;
+   // int N = 27;
+    int aBet[27];
+    int k = 65;
     
+    for(i = 0; i < 26; i++)
+    {
+        aBet[i] = k+i;
+        printf("%c", (char)(aBet[i]));
+    }
     
     return 0;
 }
